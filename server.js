e50210faeb65b6c15447004e3198b9ef9c7ae859
Martti Malmi <sirius@iki.fi>
2016-12-09 21:26:30 +0200

    enable cors

--- server.js
+++ server.js
@@ -97,6 +97,13 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = config.get('port');
 
 // Routes
